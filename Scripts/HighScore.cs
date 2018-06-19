23f8e8697c9e7d675a4bc9a455c9130beea35204
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2018-06-20 00:00:41 +0530

    Add files via upload

---
+++ Scripts/HighScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour {
+	
+	public string[] names;
+
+	void OnEnable(){
+		Debug.Log ("I am Enabled");
+	}
+	void OnDisable(){
+		
+		Debug.Log ("The people who work at Razeware are: ");
+		Debug.Log (names [0]);
+		Debug.Log (names [1]);
+		Debug.Log (names [2]);
+		Debug.Log (names [3]);
+		Debug.Log (names [4]);
+	}
+}
---
+++ Scripts/TipCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipCalculator : MonoBehaviour {
+	//Defining the variables to be used
+	public double balance = 2000;
+	public double percentageOfTheTip=2;
+	public double tip;
+	//Defining the function to be performed on click Enable
+	void OnEnable(){
+		Debug.Log ("I am Enabled");
+	}
+	//Defining the function to be performed on click Disable
+	void OnDisable(){
+		tip = ((balance / 100) * percentageOfTheTip);
+		Debug.Log ("Tip = " + tip);
+	}
+
+}
